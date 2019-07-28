6bfbacebd28ac492596d168b9c2a5772cdcef226
sukaidev <suhaihong88@gmail.com>
2019-07-28 13:27:08 +0800

    封装Retrofit

--- common/src/main/java/com/sukaidev/common/http/HttpHelper.kt
+++ common/src/main/java/com/sukaidev/common/http/HttpHelper.kt
@@ -1,5 +1,6 @@
 package com.sukaidev.common.http
 
+import com.sukaidev.common.common.Constant
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
---
+++ common/src/main/java/com/sukaidev/common/http/entity/Think.kt
@@ -0,0 +1,14 @@
+package com.sukaidev.common.http.entity
+
+/**
+ * Created by sukaidev on 2019/07/28.
+ *
+ */
+
+/**
+ * 通用数据返回实现
+ * @status:响应状态码
+ * @message:响应文字消息
+ * @data:具体响应业务对象
+ */
+data class Think<out T>(val status: Int, val message: String, val data: T)
